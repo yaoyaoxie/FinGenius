0a20480a05a773e6ad48b166fbca2fb15efa5e09
huayao <youzixiaoyaojing@163.com>
2025-03-25 09:52:03 +0000

    add openmanus agent

---
+++ src/agent/__init__.py
@@ -0,0 +1,10 @@
+from src.agent.base import BaseAgent
+from src.agent.react import ReActAgent
+from src.agent.toolcall import ToolCallAgent
+
+
+__all__ = [
+    "BaseAgent",
+    "ReActAgent",
+    "ToolCallAgent",
+]
---
+++ src/tool/__init__.py
@@ -0,0 +1,12 @@
+from src.tool.base import BaseTool
+from src.tool.create_chat_completion import CreateChatCompletion
+from src.tool.terminate import Terminate
+from src.tool.tool_collection import ToolCollection
+
+
+__all__ = [
+    "BaseTool",
+    "Terminate",
+    "ToolCollection",
+    "CreateChatCompletion",
+]
