a04144494ce7c776f288f99b5816ac5aea21e821
huayao <youzixiaoyaojing@163.com>
2025-04-01 03:29:13 +0000

    add WebSearch Tool

---
+++ src/tool/search/__init__.py
@@ -0,0 +1,14 @@
+from src.tool.search.baidu_search import BaiduSearchEngine
+from src.tool.search.base import WebSearchEngine
+from src.tool.search.bing_search import BingSearchEngine
+from src.tool.search.duckduckgo_search import DuckDuckGoSearchEngine
+from src.tool.search.google_search import GoogleSearchEngine
+
+
+__all__ = [
+    "WebSearchEngine",
+    "BaiduSearchEngine",
+    "DuckDuckGoSearchEngine",
+    "GoogleSearchEngine",
+    "BingSearchEngine",
+]
